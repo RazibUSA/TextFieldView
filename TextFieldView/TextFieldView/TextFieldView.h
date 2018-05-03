5eed33fcf2f2440e122d2f14e60489542b8688ad
Mollick, Md Razib Uddin <MMollick@Ashleyfurniture.com>
2018-05-03 09:28:03 -0400

    Init Framework

---
+++ TextFieldView/TextFieldView/TextFieldView.h
@@ -0,0 +1,19 @@
+//
+//  TextFieldView.h
+//  TextFieldView
+//
+//  Created by Mollick, Md Razib Uddin on 5/2/18.
+//  Copyright © 2018 Rio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TextFieldView.
+FOUNDATION_EXPORT double TextFieldViewVersionNumber;
+
+//! Project version string for TextFieldView.
+FOUNDATION_EXPORT const unsigned char TextFieldViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TextFieldView/PublicHeader.h>
+
+
